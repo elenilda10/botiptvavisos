a6916619babd14d71f79e2e030e5f25022b147bc
motorcell33-crypto <motorcell33@gmail.com>
2026-09-10 21:45:50 -0300

    Create scheduledHandler.js

---
+++ src/handlers/scheduledHandler.js
@@ -0,0 +1,8 @@
+export async function handleScheduled(env, controller) {
+  console.log(
+    "[CRON] Execução agendada recebida:",
+    new Date().toISOString()
+  );
+
+  // Depois vamos ligar aqui os banners automáticos.
+}
